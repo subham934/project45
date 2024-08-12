4f62995c55abbe1bd54811c3aa1a90dd8b50cd05
Subham Dhar <43316699+subham934@users.noreply.github.com>
2024-08-12 21:07:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('#menu-icon')
+let navlist = document.querySelector('.navlist')
+
+
+menu.addEventListener('click', function(){
+    navlist.classList.toggle('active')
+})
+
+window.onscroll = ()=>{
+    navlist.classList.remove('active')
+
+}
